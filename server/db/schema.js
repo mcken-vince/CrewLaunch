8c03a8eab0ab2860fa833e8f6e4f84d9bd55707e
mcken-vince <mcken.vince@gmail.com>
2021-10-14 13:02:47 -0600

    Wipe models clean and create seeds in schema file for now

--- server/db/schema.js
+++ server/db/schema.js
@@ -9,8 +9,11 @@ const crewSchema = new mongoose.Schema({
     type: Number,
     required: true
   },
-  avatar: String,
-  is_active: Boolean
+  is_active: {
+    type: Boolean,
+    default: true
+  },
+  avatar: String
 }, { timestamps: true });
 
 const clientSchema = new mongoose.Schema({
@@ -95,11 +98,62 @@ const Package = mongoose.model('Package', packageSchema);
 const Contract = mongoose.model('Contract', contractSchema);
 const Job = mongoose.model('Job', jobSchema);
 
-const cli = new Client({
-  name: 'Gregory Peck',
-  phone: '403-552-9094',
-  email: 'regreg@peck.com'
+
+Promise.all([
+  Crew.deleteMany({}),
+  Client.deleteMany({}),
+  Package.deleteMany({}),
+  Contract.deleteMany({}),
+  Job.deleteMany({})
+])
+.then(() => {
+  console.log('Models wiped squeaky clean!')
+  // Some seeds here below
+  const cr1 = new Crew({
+    foreman_name: 'Andy Bandy',
+    crew_size: 2,
+    avatar: "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShaggyMullet&accessoriesType=Wayfarers&hairColor=Brown&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=ShirtScoopNeck&clotheColor=Red&eyeType=Happy&eyebrowType=RaisedExcitedNatural&mouthType=Twinkle&skinColor=Pale'"
+  });
+  cr1.save().then((crew1) => console.log('crew1 created: ', crew1));
+  
+  const cr2 = new Crew({
+    foreman_name: 'Heddy Ready',
+    crew_size: 4,
+    avatar: "https://getavataaars.com/?accessoriesType=Prescription02&avatarStyle=Transparent&clotheColor=PastelRed&clotheType=ShirtCrewNeck&eyeType=Squint&eyebrowType=FlatNatural&facialHairColor=BrownDark&facialHairType=BeardMajestic&graphicType=Resist&hairColor=PastelPink&mouthType=Smile&skinColor=Tanned&topType=WinterHat2"
+  });
+  cr2.save().then(console.log('crew2 created'));
+  
+  const cli1 = new Client({
+    name: 'Gregory Peck',
+    phone: '403-552-9094',
+    email: 'regreg@peck.com'
+  });
+  cli1.save().then(console.log('client1 created'));
+  
+  const pack1 = new Package({
+    title: 'Lawn Care Silver',
+    description: '3 Month, Biweekly: Mow, edge trim, blow off pathways',
+    cost: 5000,
+    contract_length_days: 90,
+    visit_interval_days: 14,
+    man_hrs_per_visit: 4
+  });
+  pack1.save().then(console.log('package1 created'));
+  
+  const pack2 = new Package({
+    title: 'Lawn Care Gold',
+    description: 'Two week, Daily: Mow, edge trim, blow off pathways',
+    cost: 5000,
+    contract_length_days: 14,
+    visit_interval_days: 1,
+    man_hrs_per_visit: 5
+  });
+  
+  pack2.save().then(console.log('package2 created'));
+})
+.catch((err) => {
+  console.log('Error resetting database: ', err);
 });
-cli.save().then(() => console.log('one entry added'));
+
 
 module.exports = { Crew, Client, Package, Contract, Job };
