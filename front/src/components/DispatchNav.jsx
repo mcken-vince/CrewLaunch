22244edf533fd5425efd7560494041e3181a5ba1
mcken-vince <mcken.vince@gmail.com>
2021-10-14 16:03:33 -0600

    Cleanup DispatchNav component

--- front/src/components/DispatchNav.jsx
+++ front/src/components/DispatchNav.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import '../styles/DispatchNav.scss';
 
 const DispatchNav = (props) => {
-  
+  const { user } = props;
   return (
     <nav className="dispatch-nav">
       <span className="dispatch-nav-logo">CrewLauncher</span>
       <div className="dispatch-nav-right">
-        {props.user ? (
+        {user ? (
           <button>Log Out</button>) : (
           <>
             <button>Log In</button>
             <button>Sign Up</button>
           </>)
         }
-        
       </div>
     </nav>
   );
