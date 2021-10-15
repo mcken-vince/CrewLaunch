59c813a30f4ecb72e3f9d9e0b2d07e5a605464de
mcken-vince <mcken.vince@gmail.com>
2021-10-14 20:16:05 -0600

    Use crews, jobs, and packages routes in server.js

--- server/server.js
+++ server/server.js
@@ -28,9 +28,16 @@ mongoose.connect(
 const apiRoutes = require('./routes/apiRoutes');
 const clientsRoutes = require('./routes/clientsRoutes');
 const contractsRoutes = require('./routes/contractsRoutes');
+const crewsRoutes = require('./routes/crewsRoutes');
+const jobsRoutes = require('./routes/jobsRoutes');
+const packagesRoutes = require('./routes/packagesRoutes');
+
 App.use('/api', apiRoutes(models));
 App.use('/clients', clientsRoutes(models.Client));
 App.use('/contracts', contractsRoutes(models.Contract));
+App.use('/crews', crewsRoutes(models.Crew));
+App.use('/jobs', jobsRoutes(models.Job));
+App.use('/packages', packagesRoutes(models.Package));
 
 App.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
