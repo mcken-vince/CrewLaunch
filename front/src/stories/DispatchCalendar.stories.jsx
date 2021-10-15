122e061eee7dfc2654257a64c466a3246db42723
mcken-vince <mcken.vince@gmail.com>
2021-10-14 21:01:25 -0600

    Plug existing components into storybook

---
+++ front/src/stories/DispatchCalendar.stories.jsx
@@ -0,0 +1,10 @@
+import DispatchCalendar from "../components/DispatchCalendar";
+
+export default {
+  title: 'Dispatch/DispatchCalendar',
+  component: DispatchCalendar
+};
+
+const Template = (args) => <DispatchCalendar {...args} />;
+
+export const Calendar = Template.bind({});
