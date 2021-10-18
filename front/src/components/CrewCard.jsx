f3ed44d2d8ca0aa965ee18451fb0a0ad9e92b9b0
mcken-vince <mcken.vince@gmail.com>
2021-10-18 14:00:01 -0600

    Style crew card, add story for card with jobs

--- front/src/components/CrewCard.jsx
+++ front/src/components/CrewCard.jsx
@@ -1,15 +1,26 @@
 import Image from 'react-bootstrap/Image';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/CrewCard.scss';
+import format from 'date-fns/format';
+import classNames from 'classnames';
 
 const CrewCard = (props) => {
   const { foreman_name, crew_size, avatar } = props.crew;
 
+  const jobs = props.jobs.map(job => {
+    const jobClass = classNames('crewCard-job', {'crewCard-job-complete': job.complete});
+    return (<h5 className={jobClass}>{job.address} - {format(job.date, 'eeee MMMM dd yyyy')}</h5>);
+  });
+
+
   return (
     <div className='crewCard-container' >
-      <Image src={avatar && avatar} alt='crew avatar' roundedCircle />
+      <Image className='crewCard-avatar' src={avatar && avatar} alt='crew avatar' roundedCircle />
       <div className='crewCard-info'>
-        <h1>{foreman_name}</h1>
-        <p>Size: {crew_size} </p>
+        <h3>{foreman_name}</h3>
+        <p>{crew_size} workers</p>
+      </div>
+      <div className='crewCard-jobs-container'>
+        {jobs}
       </div>
     </div>
   )
--- front/src/stories/CrewCard.stories.jsx
+++ front/src/stories/CrewCard.stories.jsx
@@ -17,3 +17,55 @@ WithNoJobs.args = {
   }, 
   jobs: []
 }
+
+export const WithJobs = Template.bind({});
+
+WithJobs.args = {
+  crew: {
+    foreman_name: 'Andy Bandy',
+    crew_size: 2,
+    avatar: "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShaggyMullet&accessoriesType=Wayfarers&hairColor=Brown&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=ShirtScoopNeck&clotheColor=Red&eyeType=Happy&eyebrowType=RaisedExcitedNatural&mouthType=Twinkle&skinColor=Pale'"
+  }, 
+  jobs: [
+    {
+      address: '525 Yondervan Cres SW, Calgary AB',
+      date: new Date(),
+      complete: true
+    },
+    { 
+      address: '72 Indeed Ave NW, Calgary AB',
+      date: new Date(),
+      complete: true
+    },
+    {
+      address: '525 Yondervan Cres SW, Calgary AB',
+      date: new Date(),
+      complete: true
+    },
+    { 
+      address: '72 Indeed Ave NW, Calgary AB',
+      date: new Date(),
+      complete: false
+    },
+    {
+      address: '525 Yondervan Cres SW, Calgary AB',
+      date: new Date(),
+      complete: false
+    },
+    { 
+      address: '72 Indeed Ave NW, Calgary AB',
+      date: new Date(),
+      complete: false
+    },
+    {
+      address: '525 Yondervan Cres SW, Calgary AB',
+      date: new Date(),
+      complete: false
+    },
+    { 
+      address: '72 Indeed Ave NW, Calgary AB',
+      date: new Date(),
+      complete: false
+    }
+  ]
+}
