ce44a63d996d41cafb7f6ed36f505f4a59637948
mcken-vince <mcken.vince@gmail.com>
2021-10-18 17:24:29 -0600

    Implement error/success message in package form, ready for action

--- front/src/stories/PackageForm.stories.jsx
+++ front/src/stories/PackageForm.stories.jsx
@@ -8,3 +8,17 @@ export default {
 const Template = (args) => <PackageForm {...args} />;
 
 export const EmptyForm = Template.bind({});
+
+export const EditForm = Template.bind({});
+
+EditForm.args = {
+  editPackage: {
+    title: 'Sweet Lawn Care',
+    description: 'Mow, edge trim, and we will blow your leaves all over the place!',
+    cost: 5000,
+    interval: 7,
+    contractLength: 30,
+    timeEst: 3
+  },
+  onSubmit: ()=> new Promise((res, rej) => res('yay'))
+};
