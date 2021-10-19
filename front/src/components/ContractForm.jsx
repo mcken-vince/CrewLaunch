f978faa581d6bdfd96ed61cdaaa9bb95de4e92d9
mcken-vince <mcken.vince@gmail.com>
2021-10-19 10:02:47 -0600

    Implement Offcanvas package display in contract form

--- front/src/components/ContractForm.jsx
+++ front/src/components/ContractForm.jsx
@@ -3,13 +3,14 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Alert from 'react-bootstrap/Alert';
-import format from 'date-fns/format';
 import addDays from 'date-fns/addDays';
 import { useState } from 'react';
 import DateRangePicker from './DateRangePicker';
+import PackagesOffcanvas from './PackagesOffcanvas';
 
 const ContractForm = (props) => {
-  const { onSubmit } = props;
+  const { packages, onSubmit } = props;
+  const [packagesShow, setPackagesShow] = useState(false);
   const [selectedPackage, setSelectedPackage] = useState(props.editContract && props.editContract.selectedPackage ? props.editContract.selectedPackage : null);
   const [client, setClient] = useState(props.editContract && props.editContract.client ? props.editContract.client : {name: '', email: '', phone: ''});
   const [address, setAddress] = useState(props.editContract && props.editContract.address ? props.editContract.address : '');
@@ -23,12 +24,9 @@ const ContractForm = (props) => {
   const formFilled = selectedPackage && client.name && client.email && address && startDate;
 
   const handleDateChange = (date) => {
+    console.log(`handleDateChange: \n newStartDate: ${date} \n newEndDate:  ${addDays(date, parseInt(selectedPackage.contract_length_days) - 1)}`)
     setStartDate(date);
-    setEndDate(selectedPackage ? addDays(date, selectedPackage.contract_length_days - 1) : date);
-  };
-
-  const openPackages = () => {
-
+    setEndDate(addDays(new Date(date), parseInt(selectedPackage.contract_length_days) - 1));
   };
 
   const validate = () => {
@@ -60,38 +58,41 @@ const ContractForm = (props) => {
   };
 
   return (
-  <div className='contract-form-container'>
-    <h1 className='contract-form-title'>Contract Form</h1>
-    {alert.success && <Alert className='contract-form-alert' variant='success'>Great success!</Alert>}
-    {alert.error && <Alert className='contract-form-alert'variant='danger'>Error: Your request could not be completed. Please try again.</Alert>}
-    <Form className='contract-form'>
-      <Form.Label>Client Information:</Form.Label>
-      <InputGroup className='mb-3'>
-        <Form.Control type='text' value={client.name} onChange={(e) => setClient(prev => ({...prev, name: e.target.value}))} placeholder='Enter name'/>
-        <Form.Control type='email' value={client.email} onChange={(e) => setClient(prev => ({...prev, email: e.target.value}))} placeholder='Enter email'/>
-        <Form.Control type='text' value={client.phone} onChange={(e) => setClient(prev => ({...prev, phone: e.target.value}))} placeholder='Enter phone #'/>
-      </InputGroup>
+    <>
+      <PackagesOffcanvas show={packagesShow} handleClose={() => setPackagesShow(false)} packages={packages} selectPackage={setSelectedPackage} />
+      <div className='contract-form-container'>
+        <h1 className='contract-form-title'>Contract Form</h1>
+        {alert.success && <Alert className='contract-form-alert' variant='success'>Great success!</Alert>}
+        {alert.error && <Alert className='contract-form-alert'variant='danger'>Error: Your request could not be completed. Please try again.</Alert>}
+        <Form className='contract-form'>
+          <Form.Label>Client Information:</Form.Label>
+          <InputGroup className='mb-3'>
+            <Form.Control type='text' value={client.name} onChange={(e) => setClient(prev => ({...prev, name: e.target.value}))} placeholder='Enter name'/>
+            <Form.Control type='email' value={client.email} onChange={(e) => setClient(prev => ({...prev, email: e.target.value}))} placeholder='Enter email'/>
+            <Form.Control type='text' value={client.phone} onChange={(e) => setClient(prev => ({...prev, phone: e.target.value}))} placeholder='Enter phone #'/>
+          </InputGroup>
 
-      <Form.Group className='mb-3' controlId='contractFormAddress'>
-        <Form.Label>Address:</Form.Label>
-        <Form.Control type='text' value={address} onChange={(e) => setAddress(e.target.value)} placeholder='Enter address' /> 
-      </Form.Group>
-      
-      <InputGroup className='mb-3'>
-        <Form.Control type='text' readOnly={true} value={selectedPackage ? selectedPackage.title : 'Please select a package'} />
-        <Button onClick={openPackages}>Select Package</Button>
-      </InputGroup>
+          <Form.Group className='mb-3' controlId='contractFormAddress'>
+            <Form.Label>Address:</Form.Label>
+            <Form.Control type='text' value={address} onChange={(e) => setAddress(e.target.value)} placeholder='Enter address' /> 
+          </Form.Group>
+          
+          <InputGroup className='mb-3'>
+            <Form.Control type='text' readOnly={true} value={selectedPackage ? `${selectedPackage.title} - $${selectedPackage.cost} - ${selectedPackage.contract_length_days} days` : 'Please select a package'} />
+            <Button onClick={() => setPackagesShow(true)}>Select Package</Button>
+          </InputGroup>
 
-      {selectedPackage && 
-      <>
-        <Form.Label>Select Contract Start Date:</Form.Label>
-        <DateRangePicker startDate={new Date(startDate)} endDate={new Date(endDate)} onChange={handleDateChange} inheritClassName='contract-form-daterangepicker'/>
-      </>}
+          {selectedPackage && 
+          <>
+            <Form.Label>Select Contract Start Date:</Form.Label>
+            <DateRangePicker startDate={new Date(startDate)} endDate={new Date(endDate)} onChange={handleDateChange} inheritClassName='contract-form-daterangepicker'/>
+          </>}
 
-      <Button disabled={!loading && !formFilled} type="submit" onClick={validate}>Submit</Button>
+          <Button disabled={!loading && !formFilled} type="submit" onClick={validate}>Submit</Button>
 
-    </Form>
-  </div>
+        </Form>
+      </div>
+    </>
   );
 };
 
