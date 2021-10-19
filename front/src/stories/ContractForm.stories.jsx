c7d85312cd39ca2770b57f6cfc3e8f004164f1a2
mcken-vince <mcken.vince@gmail.com>
2021-10-19 10:03:08 -0600

    Update contract form stories to pass packages and correct props

--- front/src/stories/ContractForm.stories.jsx
+++ front/src/stories/ContractForm.stories.jsx
@@ -8,3 +8,24 @@ export default {
 const Template = (args) => <ContractForm {...args} />;
 
 export const EmptyForm = Template.bind({});
+EmptyForm.args = {
+  packages: [
+    {
+      title: 'Lawn Care Silver',
+      description: '3 Month, Biweekly: Mow, edge trim, blow off pathways',
+      cost: 5000,
+      contract_length_days: 90,
+      visit_interval_days: 14,
+      man_hrs_per_visit: 4
+    },
+    {  
+      title: 'Lawn Care Gold',
+      description: 'Two week, Daily: Mow, edge trim, blow off pathways',
+      cost: 5000,
+      contract_length_days: 14,
+      visit_interval_days: 1,
+      man_hrs_per_visit: 5
+    }
+  ]
+}
+
