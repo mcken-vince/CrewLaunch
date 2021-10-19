e04db8d4b7be4b6d28de71bc9f9e0d4fd06de22c
mcken-vince <mcken.vince@gmail.com>
2021-10-19 14:42:30 -0600

    Use effect in App to fetch data and set state object

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,39 @@
 import './App.scss';
 import { Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [state, setState] = useState({});
+  
+  useEffect(() => {
+    Promise.all([
+      axios.get('/api/crews'),
+      axios.get('/api/clients'),
+      axios.get('/api/packages'),
+      axios.get('/api/contracts'),
+      axios.get('/api/jobs')
+    ])
+    .then(data => {
+      const newState = {
+        crews: data[0].data.result,
+        clients: data[1].data.result,
+        packages: data[2].data.result,
+        contracts: data[3].data.result,
+        jobs: data[4].data.result
+      };
+      console.log('new state: ', newState);
+      setState(newState);
+    })
+    .catch(err => console.log('Error fetching data!', err));
+  }, []);
+
+
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path='/crews/:id'>
-            
-          </Route>
-        </Switch>
-      </Router>
+      <h1>{state.crews && state.crews[0].foreman_name}</h1>
+      
     </div>
   );
 }
