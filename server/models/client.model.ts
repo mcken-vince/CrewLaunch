fe8f792b8db3b9788272522dc1c8b809537e986d
mcken-vince <mcken.vince@gmail.com>
2021-10-20 14:31:17 -0600

    Create client model

---
+++ server/models/client.model.ts
@@ -0,0 +1,22 @@
+import { Types, Document, Schema, model} from 'mongoose';
+
+export interface IClient extends Document {
+  _id?: Types.ObjectId;
+  phone?: string;
+  name: string;
+  email: string;
+};
+
+const ClientSchema = new Schema<IClient>({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: String
+}, { timestamps: true });
+
+export const ClientModel = model<IClient>('Client', ClientSchema);
