69ef84d1fb980c0c9fb875f84483ba40fd893cc4
mcken-vince <mcken.vince@gmail.com>
2021-10-19 18:33:46 -0600

    Fix routes, reinstate data context providers around app

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,4 @@
-import { Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import "./App.scss";
 import React, { useEffect, useContext } from "react";
 import useAppData from "./hooks/useAppData";
@@ -11,33 +11,32 @@ function App() {
   const DataUpdate = React.createContext();
 
   return (
-    <Router>
-      <Data.Provider value={state}>
-        <DataUpdate.Provider value={updateState}>
-      <Switch>
-        <div className="App">
+    <div className="App">
+      <Router>
+        <Data.Provider value={state}>
+          <DataUpdate.Provider value={updateState}>
+            <Switch>
+              <Route path="/crews/:id">
+                <CrewsDashboardPage />
+              </Route>
 
-        <Route path='/crews/:id' >
-          <CrewsDashboardPage />
-        </Route>
-        
-        <Route path='/dispatch' >
-          <DispatchDashboardPage />
-        </Route> 
+              <Route path="/dispatch">
+                <DispatchDashboardPage />
+              </Route>
 
-        <Route path='/'>
-          <h1>Welcome to Crew Launcher</h1>
-          <Link to='/dispatch'>Dispatch</Link>
-          <Link to='/crews/1'>Crew #1</Link>
-          <Link to='/crews/2'>Crew #2</Link>
-          
-            <p>{JSON.stringify(state)}</p>
-        </Route>
-        </div>
-      </Switch>
-        </DataUpdate.Provider>
-      </Data.Provider>
-    </Router>
+              <Route path="/">
+                <h1>Welcome to Crew Launcher</h1>
+                <Link to="/dispatch">Dispatch</Link>
+                <Link to="/crews/1">Crew #1</Link>
+                <Link to="/crews/2">Crew #2</Link>
+
+                <p>{JSON.stringify(state)}</p>
+              </Route>
+            </Switch>
+          </DataUpdate.Provider>
+        </Data.Provider>
+      </Router>
+    </div>
   );
 }
 
