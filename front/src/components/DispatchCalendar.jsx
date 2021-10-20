7d5dc8a57d24ce8a9576af9ed3dac30871f0b65e
mcken-vince <mcken.vince@gmail.com>
2021-10-19 21:13:30 -0600

    Create weekday labels on calendar, still needs lots of work

--- front/src/components/DispatchCalendar.jsx
+++ front/src/components/DispatchCalendar.jsx
@@ -10,16 +10,20 @@ const DispatchCalendar = () => {
     days: getDaysInMonth(today)
   };
 
+  const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const daysOfWeek = week.map(wd => <div className='weekday-container'>{wd}</div>)
+
   const dayCards = [];
   for (let d = 1; d <= thisMonth.days; d++) {
     const dayOfMonth = (0 < d && d < 10) ? `0${d}` : `${d}`;
-    dayCards.push(<DayCard date={dayOfMonth} key={d}/>)
+    dayCards.push(<DayCard date={dayOfMonth} key={d}/>);
   }
 
   return (
     <div className='dispatch-calendar-container'>
       <h1>{`${thisMonth.name} ${thisMonth.year}`}</h1>
       <div className='dispatch-calendar-days-container'>
+        {daysOfWeek}
         {dayCards}
       </div>
     </div>
