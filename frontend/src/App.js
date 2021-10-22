8ad71d60550280e9c972a42a461d10278210e6f4
mcken-vince <mcken.vince@gmail.com>
2021-10-21 20:09:31 -0600

    Revert back to App.js, will start with converting components to typescript

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import "./App.scss";
-import React, { useEffect, useContext, FC } from "react";
+import React, { useEffect, useContext } from "react";
 import useAppData from "./hooks/useAppData";
 import CrewsDashboardPage from './components/pages/CrewsDashboardPage';
 import DispatchDashboardPage from './components/pages/DispatchDashboardPage';
 
+
 function App() {
   const {state, updateState} = useAppData();
-  const Data = React.createContext();
-  const DataUpdate = React.createContext();
+  // const Data = React.createContext(state);
+  // const DataUpdate = React.createContext(updateState);
 
   return (
     <div className="App">
       <Router>
-        <Data.Provider value={state}>
-          <DataUpdate.Provider value={updateState}>
+
             <Switch>
               <Route path="/crews/:id">
                 <CrewsDashboardPage />
@@ -33,11 +33,10 @@ function App() {
                 <p>{JSON.stringify(state, null, 3)}</p>
               </Route>
             </Switch>
-          </DataUpdate.Provider>
-        </Data.Provider>
+
       </Router>
     </div>
   );
-}
+};
 
 export default App;
