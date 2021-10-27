5da46d56064aa04344192e64e93406e535ae25c4
mcken-vince <mcken.vince@gmail.com>
2021-10-27 10:50:40 -0600

    Fix bug, remove second Router, now NavLinks are working

--- frontend/src/components/pages/DispatchDashboardPage.tsx
+++ frontend/src/components/pages/DispatchDashboardPage.tsx
@@ -4,9 +4,10 @@ import '../../styles/DispatchDashboardPage.scss';
 import DispatchCalendar from '../DispatchCalendar';
 import DispatchNav from '../DispatchNav';
 import { getJobsWithDetails } from '../../helpers/dataCombiners';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
 
 const DispatchDashboardPage: FC<any> = (props): ReactElement => {
+  const { url } = useRouteMatch();
   const state: IState = props.state;
 
   const detailedJobs = state.jobs ? getJobsWithDetails(state.jobs, state.contracts, state.packages) : [];
@@ -14,21 +15,21 @@ const DispatchDashboardPage: FC<any> = (props): ReactElement => {
     <>
       <DispatchNav user={{name: 'Testy Tester', email: 'testy.testerforce@mail.com', password: 'secretpassword' }}/>
       <div className='dispatch-dashboard-container'> 
-        <Router>
+
           <Switch>
-            <Route path='/dispatch/crews/new'>
+            <Route path={`${url}/crews/new`}>
               <h1>New Crew Form</h1>
             </Route>
-            <Route path='/dispatch/packages/new'>
+            <Route path={`${url}/packages/new`}>
               <h1>New Package Form</h1>
             </Route>
-            <Route path='/dispatch/'>
+
+
+            <Route path={`${url}`}>
               <DispatchCalendar jobs={detailedJobs}/>
             </Route>
-            
-
           </Switch>
-        </Router>
+
       </div>
     </>
   );
