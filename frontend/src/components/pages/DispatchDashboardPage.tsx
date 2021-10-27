373a2d5b8b9fda9d0e395f8276847f2831ca5c8a
mcken-vince <mcken.vince@gmail.com>
2021-10-27 10:16:52 -0600

    Display navbar on dispatch dashboard, add padding to dashboard container to move out from under navbar

--- frontend/src/components/pages/DispatchDashboardPage.tsx
+++ frontend/src/components/pages/DispatchDashboardPage.tsx
@@ -2,6 +2,7 @@ import { FC, ReactElement } from 'react';
 import { IState } from '../../definitions';
 import '../../styles/DispatchDashboardPage.scss';
 import DispatchCalendar from '../DispatchCalendar';
+import DispatchNav from '../DispatchNav';
 import { getJobsWithDetails } from '../../helpers/dataCombiners';
 
 const DispatchDashboardPage: FC<any> = (props): ReactElement => {
@@ -10,8 +11,10 @@ const DispatchDashboardPage: FC<any> = (props): ReactElement => {
   const detailedJobs = state.jobs ? getJobsWithDetails(state.jobs, state.contracts, state.packages) : [];
   return (
     <>
-      <h1>DispatchDashboardPage</h1>
-      <DispatchCalendar jobs={detailedJobs}/>
+      <DispatchNav user={{name: 'Testy Tester', email: 'testy.testerforce@mail.com', password: 'secretpassword' }}/>
+      <div className='dispatch-dashboard-container'> 
+        <DispatchCalendar jobs={detailedJobs}/>
+      </div>
     </>
   );
 };
