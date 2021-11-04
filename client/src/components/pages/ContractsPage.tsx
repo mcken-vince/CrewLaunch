fd8cbcd321be0af87782c5076e9a0c437fe19554
mcken-vince <mcken.vince@gmail.com>
2021-11-04 16:33:07 -0600

    Create skeleton for ContractsPage component

---
+++ client/src/components/pages/ContractsPage.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactElement } from "react";
+
+
+const ContractsPage: FC<ContractsPageProps> = (props): ReactElement => {
+
+  return (
+    <h1>Contracts:</h1>
+  );
+};
+
+export default ContractsPage;
+
+interface ContractsPageProps {
+  
+};
