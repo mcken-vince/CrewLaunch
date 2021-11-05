65640e82221f7fadaf5df49cc83cf618fcef38cf
mcken-vince <mcken.vince@gmail.com>
2021-11-04 21:27:22 -0600

    Implement CrewsPage, add CustomSearchBar and filter by foreman_name

--- client/src/components/pages/CrewsPage.tsx
+++ client/src/components/pages/CrewsPage.tsx
@@ -1,13 +1,26 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useState } from "react";
 import { ICrew } from "../../definitions";
-
+import '../../styles/CrewsPage.scss';
+import CrewCard from "../CrewCard";
+import CustomSearchBar from "../CustomSearchBar";
 
 const CrewsPage: FC<CrewsPageProps> = (props): ReactElement => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
   const crews = props.crews;
+
+  const filteredCrews = searchTerm ? crews.filter(c => {
+    return c.foreman_name.toLowerCase().includes(searchTerm.toLowerCase());
+  }) : crews;
+
+  const crewCards = filteredCrews.map((c, idx) => {
+    return <CrewCard crew={c} key={idx} jobs={[]}/>;
+  });
 
   return (
     <div className='crews-container'>
       <h1>Crews: {crews.length}</h1>
+      <CustomSearchBar value={searchTerm} onChange={setSearchTerm} placeholder='Search by foreman name'/>
+      {crewCards}
     </div>
   );
 };
