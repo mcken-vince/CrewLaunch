81d45f632419e45ced32029f6466cd7c758f6a95
mcken-vince <mcken.vince@gmail.com>
2021-11-04 20:37:47 -0600

    Build out contracts page, searchbar works

--- client/src/components/pages/ContractsPage.tsx
+++ client/src/components/pages/ContractsPage.tsx
@@ -1,12 +1,18 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useState } from "react";
+import { FormControl, InputGroup, Button } from "react-bootstrap";
 import '../../styles/ContractsPage.scss';
 import { IContractLocal } from "../component-types"; 
 import ContractCard from "../ContractCard";
 
 const ContractsPage: FC<ContractsPageProps> = (props): ReactElement => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
   const contracts: IContractLocal[] = props.contracts;
 
-  const contractCards = contracts.map((contract, idx) => {
+  const filteredContracts = searchTerm ? contracts.filter(c => {
+    return c.address.toLowerCase().includes(searchTerm.toLowerCase());
+  }) : contracts;
+
+  const contractCards = filteredContracts.map((contract, idx) => {
     return <ContractCard key={idx} contract={contract}/>
   });
 
@@ -14,7 +20,21 @@ const ContractsPage: FC<ContractsPageProps> = (props): ReactElement => {
     <>
       <div className='contracts-container'>
         <h1>Contracts:</h1>
-        {contractCards}
+        
+        <div className='contracts-search'>
+        <InputGroup className='mb-3'>
+          <InputGroup.Text id='search'>Search By Address:</InputGroup.Text>
+          <FormControl
+            aria-describedby='search-term'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          </InputGroup>
+          {
+            contractCards && contractCards.length > 0 ? 
+            contractCards : <h2>No matching contracts found.</h2>
+          }
+        </div>
       </div>
     </>
   );
