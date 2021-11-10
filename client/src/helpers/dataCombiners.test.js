9cbc64fc24ee592c4193a4f2150dc577f63dcd81
mcken-vince <mcken.vince@gmail.com>
2021-11-10 09:22:49 -0700

    Write basic tests for dataCombiners helper functions

---
+++ client/src/helpers/dataCombiners.test.js
@@ -0,0 +1,25 @@
+import { getJobsWithDetails, getContractsWithDetails } from './dataCombiners';
+import { sampleState } from '../tests/sampleData';
+
+const { jobs, contracts, crews, packages, clients } = sampleState;
+
+describe('getJobsWithDetails helper function', () => {
+  it("should return an array of jobs with 'address', 'jobNotes', and 'servicePackage' properties", () => {
+    const result = getJobsWithDetails(jobs, contracts, packages);
+    expect(result).toBeInstanceOf(Array);
+    expect(typeof result[0].address).toBe('string');
+    expect(typeof result[0].jobNotes).toBe('string');
+    expect(result[0].servicePackage).not.toBe(undefined);
+  });
+});
+
+describe('getContractsWithDetails helper function', () => {
+  it("should return an array of contracts with 'client' and 'selectedPackage' properties", () => {
+    const result = getContractsWithDetails(contracts, packages, clients);
+    const firstContractClient = result[0].client;
+    expect(result).toBeInstanceOf(Array);
+    expect(typeof result[0].selectedPackage).not.toBe(undefined);
+    expect(typeof firstContractClient.email).toBe('string');
+    expect(typeof firstContractClient.name).toBe('string');
+  });
+});
