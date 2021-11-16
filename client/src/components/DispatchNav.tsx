65ca936f30af3410562d931d2830aec1e2905339
mcken-vince <mcken.vince@gmail.com>
2021-11-16 16:35:52 -0700

    Adjust DispatchNav dropdowns to align to end, preventing horizontal scrollbar

--- client/src/components/DispatchNav.tsx
+++ client/src/components/DispatchNav.tsx
@@ -38,13 +38,13 @@ const DispatchNav: FC<DispatchNavProps> = (props): ReactElement => {
         </div>
           {user && user.email && 
           <div className='dispatch-nav-dropdowns'>
-            <NavDropdown title={<span className='dispatch-nav-dropdown'>Forms</span>} >
+            <NavDropdown title={<span className='dispatch-nav-dropdown'>Forms</span>} align='end'>
                 <Nav.Link as={NavLink} to='/dispatch/contracts/new'>New Contract</Nav.Link>
                 <Nav.Link as={NavLink} to='/dispatch/packages/new'>New Package</Nav.Link>
                 <Nav.Link as={NavLink} to='/dispatch/crews/new'>Add a Crew</Nav.Link>
             </NavDropdown>
             
-            <NavDropdown title={<span className='dispatch-nav-dropdown'>Resources</span>} >
+            <NavDropdown title={<span className='dispatch-nav-dropdown'>Resources</span>} align='end' >
                 <Nav.Link as={NavLink} to='/dispatch/clients'>Clients</Nav.Link>
                 <Nav.Link as={NavLink} to='/dispatch/jobs'>Jobs</Nav.Link>
                 <Nav.Link as={NavLink} to='/dispatch/crews'>Crews</Nav.Link>
