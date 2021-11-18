2e198806c26bb548302eef3cf6b14aa4e814b4e1
mcken-vince <mcken.vince@gmail.com>
2021-11-17 21:47:40 -0700

    Add crew reset item in CrewSelector, and display 'Change crew' or 'Select a crew' depending on if a crew is already assigned or not

--- client/src/components/CrewSelector.tsx
+++ client/src/components/CrewSelector.tsx
@@ -1,25 +1,27 @@
 import { ICrew } from "../definitions";
 import Dropdown from "react-bootstrap/Dropdown";
 import '../styles/CrewSelector.scss';
-import { useState } from "react";
 import { Image } from "react-bootstrap";
 
 const CrewSelector = (props: CrewSelectorProps) => {
-  const { crews, onSelect } = props;
+  const { crews, onSelect, selectedCrew } = props;
 
   const crewItems = crews.map(c => {
     return (
       <Dropdown.Item onClick={() => onSelect(c)}>
-        <Image src={c.avatar} alt={c.foreman_name + "'s avatar"} />
+        <Image className='crew-image' src={c.avatar} alt={c.foreman_name + "'s avatar"} />
         <p>{c.foreman_name} - {c.crew_size} workers</p>
-      </Dropdown.Item>)
-  })
+      </Dropdown.Item>);
+  });
 
   return (
     <div className='crew-selector'>
       <Dropdown>
-        <Dropdown.Toggle>Select a crew</Dropdown.Toggle>
+        <Dropdown.Toggle>{selectedCrew ? 'Change crew' : 'Select a crew'}</Dropdown.Toggle>
         <Dropdown.Menu>
+          <Dropdown.Item onClick={() => onSelect({_id: undefined})}>
+            <p>Reset crew field</p>
+          </Dropdown.Item>
           {crewItems}
         </Dropdown.Menu>
       </Dropdown>
@@ -32,4 +34,5 @@ export default CrewSelector;
 interface CrewSelectorProps {
   crews: ICrew[];
   onSelect: Function;
+  selectedCrew: ICrew | null;
 }
