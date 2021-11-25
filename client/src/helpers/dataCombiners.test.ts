9b66c28d6ec1ff2e16d7e843a578d43f7f53572a
mcken-vince <mcken.vince@gmail.com>
2021-11-25 13:30:40 -0700

    Write unit tests for getClientsWithContracts() and getCrewJobsWithDetails() helper functions

--- client/src/helpers/dataCombiners.test.ts
+++ client/src/helpers/dataCombiners.test.ts
@@ -1,9 +1,9 @@
-import { getJobsWithDetails, getContractsWithDetails } from './dataCombiners';
+import { getJobsWithDetails, getContractsWithDetails, getClientsWithContracts, getCrewJobsWithDetails } from './dataCombiners';
 import { sampleState } from '../tests/sampleData';
 
 const { jobs, contracts, packages, clients, crews } = sampleState;
 
-describe('getJobsWithDetails helper function', () => {
+describe('getJobsWithDetails()', () => {
   it("should return an array of jobs with 'address', 'jobNotes', 'crew' and 'servicePackage' properties", () => {
     const result = getJobsWithDetails(jobs, contracts, packages, crews);
     expect(result).toBeInstanceOf(Array);
@@ -14,7 +14,7 @@ describe('getJobsWithDetails helper function', () => {
   });
 });
 
-describe('getContractsWithDetails helper function', () => {
+describe('getContractsWithDetails()', () => {
   it("should return an array of contracts with 'client' and 'selectedPackage' properties", () => {
     const result = getContractsWithDetails(contracts, packages, clients);
     const firstContractClient = result[0].client;
@@ -22,5 +22,22 @@ describe('getContractsWithDetails helper function', () => {
     expect(typeof result[0].selectedPackage).not.toBe(undefined);
     expect(typeof firstContractClient.email).toBe('string');
     expect(typeof firstContractClient.name).toBe('string');
+  });
+});
+
+describe('getClientsWithContracts()', () => {
+  it("should return an array of clients with 'contracts' property", () => {
+    const result = getClientsWithContracts(clients, contracts);
+    expect(result[0].contracts).not.toBeUndefined();
+  });
+});
+
+describe('getCrewJobsWithDetails()', () => {
+  it("should return an array of this crew's jobs with their details", () => {
+    const crewId = crews[0]._id.toString();
+    const result = getCrewJobsWithDetails(crewId, crews, jobs, packages, contracts, clients);
+    expect(result[0].crew_id).toBe(crewId);
+    expect(result[0].address).not.toBeUndefined();
+    expect(result[0].servicePackage).not.toBeUndefined();
   });
 });
