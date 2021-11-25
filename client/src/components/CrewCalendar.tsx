a2cceaf666f2174f634239ca2d74a7b15c7d45a7
mcken-vince <mcken.vince@gmail.com>
2021-11-25 10:26:51 -0700

    Begin building CrewCalendar component

---
+++ client/src/components/CrewCalendar.tsx
@@ -0,0 +1,19 @@
+import '../styles/CrewCalendar.scss';
+import DayCard from './DayCard';
+
+const CrewCalendar = (props: CrewCalendarProps) => {
+  
+
+
+  return (
+    <div className='crewcalendar-container'>
+
+    </div>
+  );
+};
+
+export default CrewCalendar;
+
+export interface CrewCalendarProps {
+
+};
