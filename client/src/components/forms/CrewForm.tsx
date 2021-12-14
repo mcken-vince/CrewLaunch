b3a2dccee6cbaf5fe4edd30d96f72b6fcf48bd64
mcken-vince <mcken.vince@gmail.com>
2021-12-14 11:47:35 -0700

    Add types to state used in CrewForm

--- client/src/components/forms/CrewForm.tsx
+++ client/src/components/forms/CrewForm.tsx
@@ -4,13 +4,13 @@ import '../../styles/CrewForm.scss';
 import { ICrew, ICrewLocal } from "../../definitions";
 
 const CrewForm: FC<CrewFormProps> = (props): ReactElement => {
-  const [foremanName, setForemanName] = useState(props.editCrew && props.editCrew.foreman_name ? props.editCrew.foreman_name : '');
-  const [crewSize, setCrewSize] = useState(props.editCrew && props.editCrew.crew_size ? props.editCrew.crew_size : 1);
-  const [avatar, setAvatar] = useState(props.editCrew && props.editCrew.avatar ? props.editCrew.avatar : '');
+  const [foremanName, setForemanName] = useState<string>(props.editCrew && props.editCrew.foreman_name ? props.editCrew.foreman_name : '');
+  const [crewSize, setCrewSize] = useState<number>(props.editCrew && props.editCrew.crew_size ? props.editCrew.crew_size : 1);
+  const [avatar, setAvatar] = useState<string>(props.editCrew && props.editCrew.avatar ? props.editCrew.avatar : '');
   const [isActive, setIsActive] = useState<boolean>(props.editCrew && props.editCrew.is_active ? props.editCrew.is_active : true);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState<boolean>(false);
   const clearAlert = {error: false, success: false};
-  const [alert, setAlert] = useState(clearAlert);
+  const [alert, setAlert] = useState<{error: boolean, success: boolean}>(clearAlert);
   const { onSubmit } = props;
   
   // is true if all required fields are not empty
