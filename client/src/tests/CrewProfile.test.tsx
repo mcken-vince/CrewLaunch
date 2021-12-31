a330bfae7e40ed0a6c40c8b923efff23ae1da840
mcken-vince <mcken.vince@gmail.com>
2021-12-31 12:13:55 -0700

    Test changing profile details in CrewProfile test

--- client/src/tests/CrewProfile.test.tsx
+++ client/src/tests/CrewProfile.test.tsx
@@ -1,11 +1,14 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import CrewProfile, { CrewProfileProps } from "../components/CrewProfile";
 import { sampleState } from "./sampleData";
 
+const crew = sampleState.crews[0];
+const editCrew = jest.fn();
+
 const renderCrewProfile = (props: Partial<CrewProfileProps> = {}) => {
   const defaultProps = {
-    crew: sampleState.crews[0],
-    editCrew: jest.fn()
+    crew,
+    editCrew
   };
 
   return render(
@@ -14,9 +17,18 @@ const renderCrewProfile = (props: Partial<CrewProfileProps> = {}) => {
 };
 
 describe('<CrewProfile/>', () => {
-  it('renders without crashing, and displays cancel button when edit button is clicked', async () => {
+  it('renders without crashing and crew details can be modified', async () => {
     renderCrewProfile();
     screen.getByTestId('edit-profile-button').click();
     expect(await screen.findByText(/Cancel/)).toBeInTheDocument();
+    // Profile is in edit mode
+    fireEvent.change(screen.getByDisplayValue(crew.foreman_name), {target: {value: 'New Name'}});
+    crew.avatar && fireEvent.change(screen.getByDisplayValue(crew.avatar), {target: {value: 'invalid avatar url'}});
+    fireEvent.change(screen.getByDisplayValue(crew.crew_size), {target: {value: 5}});
+    fireEvent.click(screen.getByRole('checkbox'));
+    fireEvent.click(screen.getByText(/Save/));
+    
+    expect(await screen.findByText(/Edit/)).toBeInTheDocument();
+    expect(editCrew).toHaveBeenCalled();
   });
 });
