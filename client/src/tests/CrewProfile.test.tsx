73b755328ef6f14fa83dc63bc60aacf690ebf0ef
mcken-vince <mcken.vince@gmail.com>
2021-12-30 17:58:21 -0700

    Create initial test for CrewProfile

---
+++ client/src/tests/CrewProfile.test.tsx
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react";
+import CrewProfile, { CrewProfileProps } from "../components/CrewProfile";
+import { sampleState } from "./sampleData";
+
+const renderCrewProfile = (props: Partial<CrewProfileProps> = {}) => {
+  const defaultProps = {
+    crew: sampleState.crews[0],
+    editCrew: jest.fn()
+  };
+
+  return render(
+    <CrewProfile {...defaultProps} {...props} />
+  );
+};
+
+describe('<CrewProfile/>', () => {
+  it('renders without crashing', () => {
+    renderCrewProfile();
+  })
+});
