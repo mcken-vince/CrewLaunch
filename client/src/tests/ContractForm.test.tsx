483ed331fec92e6b5c5f6d1326151dfd36b02442
mcken-vince <mcken.vince@gmail.com>
2021-12-31 11:07:09 -0700

    Add entry of phone number to ContractForm test

--- client/src/tests/ContractForm.test.tsx
+++ client/src/tests/ContractForm.test.tsx
@@ -30,12 +30,14 @@ describe('<ContractForm />', () => {
     renderContractForm();
     fireEvent.change(screen.getByPlaceholderText(/Enter name/), {target: {value: 'Client Name'}});
     fireEvent.change(screen.getByPlaceholderText(/Enter email/), {target: {value: 'client@email.com'}});
+    fireEvent.change(screen.getByPlaceholderText(/Enter phone #/), {target: {value: '403-929-8316'}});
     fireEvent.change(screen.getByPlaceholderText(/Enter address/), {target: {value: '123 Client St'}});
     screen.getByText(/Select Package/).click();
-    (await screen.findByTestId('selectPackage-button')).click();
+    // Wait for PackagesOffcanvas to show, then click on a select button to select a package
+    (await screen.findByTestId('select')).click();
     fireEvent.change(await screen.findByPlaceholderText(/job notes/i), {target: {value: 'Some notes'}});
     const submitButton = screen.getByTestId('submit-button');
-
+    // expect(submitButton).not.toBeDisabled();
     fireEvent.click(submitButton);
     // This validation is not working for some reason
     // expect(await screen.findByText(/success/i)).toBeInTheDocument();
