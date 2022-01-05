888203db004b96e37b7d9e10a068230299ebd7c5
mcken-vince <mcken.vince@gmail.com>
2022-01-05 11:09:17 -0700

    Use formatDate helper function instead of importing date-fns, DayCard stories updated

--- client/src/stories/DayCard.stories.jsx
+++ client/src/stories/DayCard.stories.jsx
@@ -1,5 +1,5 @@
 import DayCard from '../components/DayCard';
-import { format } from 'date-fns';
+import { formatDate } from '../helpers/dataFormatters';
 
 export default {
   title: 'CrewLauncher/DayCard',
@@ -10,16 +10,16 @@ const Template = (args) => <DayCard {...args}/>;
 
 export const EmptyDayCard = Template.bind({});
 EmptyDayCard.args = {
-  date: format(new Date(), 'eeee MMMM dd, yyyy'),
+  date: formatDate(new Date()),
   jobs: []
-}
+};
 
 export const DayCardWithJobs = Template.bind({});
 DayCardWithJobs.args = {
-  date: format(new Date(), 'eeee MMMM dd, yyyy'),
+  date: formatDate(new Date()),
   jobs: [
     {address: '52 Yardwood Drive NW, Calgary AB', complete: false},
     {address: '712 Jerry Springer Road NW, Calgary AB', complete: false},
     {address: '1900 Terrace Way NE, Calgary AB', complete: true}
   ]
-}
+};
