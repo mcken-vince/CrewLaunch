cfbcaa76f62ede56e5e3cf4605f5fa660f6c9382
mcken-vince <mcken.vince@gmail.com>
2022-01-05 11:02:55 -0700

    Update ClientCard story

--- client/src/stories/ClientCard.stories.jsx
+++ client/src/stories/ClientCard.stories.jsx
@@ -1,17 +1,23 @@
+import { BrowserRouter } from 'react-router-dom';
 import ClientCard from '../components/ClientCard';
+import { localContracts, localClients } from '../tests/sampleData';
+
 
 export default {
   title: 'CrewLauncher/ClientCard',
   component: ClientCard
 };
 
-const Template = (args) => <ClientCard {...args}/>;
+const Template = (args) => {
+  return (
+    <BrowserRouter>
+      <ClientCard {...args}/>
+    </BrowserRouter>
+  );
+};
 
 export const StandardClientCard = Template.bind({});
 StandardClientCard.args = {
-  client: {
-    name: 'Rosie Posie',
-    email: 'rosanna.posanna@mail.com',
-    phone: '403-992-1536'
-  }
+  client: localClients[0],
+  contracts: localContracts
 };
