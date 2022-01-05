aefcca637e89664dd5b668b4fc37fc3202223b58
mcken-vince <mcken.vince@gmail.com>
2022-01-05 10:58:49 -0700

    Remove commented out code

--- client/src/components/ClientCard.tsx
+++ client/src/components/ClientCard.tsx
@@ -1,7 +1,6 @@
 import { FC, ReactElement } from 'react';
 import '../styles/ClientCard.scss';
 import { IClientLocal, IContractLocal } from '../definitions';
-import { formatDate } from '../helpers/dataFormatters';
 import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 import ContractCard from './ContractCard';
@@ -11,14 +10,6 @@ const ClientCard: FC<ClientCardProps> = (props): ReactElement => {
   const { _id, name, email, phone, contracts } = props.client;
   const thisClientsContracts = props.contracts.filter(c => c.client_id.toString() === _id);
 
-  // const clientContractCards = contracts.map((c, idx) => {
-    // return (
-    //   <div key={idx} className='client-contract-container'>
-    //     <p>{c.address}</p>
-    //     <p>{formatDate(c.start_date)}</p>
-    //     <Button>View Details</Button>
-    //   </div>
-    // );
   const clientContractCards = thisClientsContracts.map((c, idx) => {
     return (
       <ContractCard key={idx} contract={c} />
