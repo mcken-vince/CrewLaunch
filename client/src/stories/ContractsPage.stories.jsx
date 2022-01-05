b1ef44944d893e47c0b6ec989e920e082ce1612c
mcken-vince <mcken.vince@gmail.com>
2022-01-05 11:35:24 -0700

    Update ContractsPage story, add text-align:center to .contracts-page-header

--- client/src/stories/ContractsPage.stories.jsx
+++ client/src/stories/ContractsPage.stories.jsx
@@ -1,4 +1,5 @@
 import ContractsPage from "../components/pages/ContractsPage";
+import { localContracts } from "../tests/sampleData";
 
 export default {
   title: 'Pages/Contracts',
@@ -9,5 +10,6 @@ const Template = (args) => <ContractsPage {...args}/>;
 
 export const Default = Template.bind({});
 Default.args = {
-  contracts: [ { "_id": "61787c58efa37244e2c35c13", "client_id": "616f7ceea703ecd4ec419646", "address": "177 Mornersome Drive SW, Calgary AB", "job_notes": "There are no notes. Nothing to see here.", "package_id": "616f7ceea703ecd4ec419647", "start_date": "2021-10-28T06:00:00.000Z" } ]
+  contracts: localContracts,
+  onDelete: () => {}
 };
